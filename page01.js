406933ce87c73f3a0a4079c93f723e2b78a27b9c
Irakli Maisuradze <82453236+Burnyco@users.noreply.github.com>
2021-04-26 13:22:17 +0400

    audio02

--- page01.js
+++ page01.js
@@ -1,27 +1,27 @@
-var mainListDiv = document.getElementById("mainListDiv"),
-    mediaButton = document.getElementById("mediaButton");
+// var mainListDiv = document.getElementById("mainListDiv"),
+//     mediaButton = document.getElementById("mediaButton");
 
-mediaButton.onclick = function() {
+// mediaButton.onclick = function() {
 
-    "use strict";
+//     "use strict";
 
-    mainListDiv.classList.toggle("show_list");
-    mediaButton.classList.toggle("active");
+//     mainListDiv.classList.toggle("show_list");
+//     mediaButton.classList.toggle("active");
 
-};
+// };
 
 
-var myIndex = 0;
-letRoll();
+// var myIndex = 0;
+// letRoll();
 
-function letRoll() {
-    var i;
-    var x = document.getElementsByClassName('Slides');
-    for (i = 0; i < x.length; i++) {
-        x[i].style.display = 'none'
-    }
-    myIndex++;
-    if (myIndex > x.length) { myIndex = 1 }
-    x[myIndex - 1].style.display = 'block';
-    setInterval(letRoll, 4000);
-}
+// function letRoll() {
+//     var i;
+//     var x = document.getElementsByClassName('Slides');
+//     for (i = 0; i < x.length; i++) {
+//         x[i].style.display = 'none'
+//     }
+//     myIndex++;
+//     if (myIndex > x.length) { myIndex = 1 }
+//     x[myIndex - 1].style.display = 'block';
+//     setInterval(letRoll, 4000);
+// }
